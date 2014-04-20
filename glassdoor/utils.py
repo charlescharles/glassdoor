a77721e4dad1786cff2c81edc23acfef48f0e810
Charles Guo <charles@addepar.com>
2014-04-20 15:09:47 -0700

    Return 'NA' from  if argument contains no numbers

--- glassdoor/utils.py
+++ glassdoor/utils.py
@@ -1,8 +1,11 @@
 import re
 
 def intify(s):
-    """Coerce string to int"""
-    return int(re.sub("[^0-9]", "", s))
+    """Coerce string to int, returning 'NA' if string contains no numbers"""
+    stripped = re.sub("[^0-9]", "", s)
+    if stripped is '':
+        return 'NA'
+    return int(stripped)
 
 def tryelse(func, default='', exception=Exception, log=False):
     """
